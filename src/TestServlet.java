98bd460808d764ad8a0d4d2557a2b9bf25081680
shagi <javashagi@yandex.ru>
2016-03-03 15:08:42 +0300

    first

---
+++ src/TestServlet.java
@@ -0,0 +1,10 @@
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by shagi on 03.03.16.
+ */
+public class TestServlet extends HttpServlet {
+    public static void main(String[] args) {
+
+    }
+}
